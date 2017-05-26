6a25b769eb96882671d2222fa1ddc47c317948c7
Water Management <water-management@mvc.local>
2017-05-26 12:42:47 -0400

    Commented generate.php/.html/.js/.css; 	~rearranged some lines in generate.js 	~removed some redundent styling in generate.css

--- water-levels-app/build/generate.js
+++ water-levels-app/build/generate.js
@@ -1,36 +1,36 @@
-var mydate=new Date()
-var theyear=mydate.getYear()
-if (theyear < 1000)
-	theyear+=1900
-var theday=mydate.getDay()
-var themonth=mydate.getMonth()+1
-if (themonth<10)
-	themonth="0"+themonth
-var theday=mydate.getDate()
-if (theday<10)
-	theday="0"+theday
+var mydate=new Date();
+
+//var day=mydate.getDay();
+var day=mydate.getDate();
+var month=mydate.getMonth() + 1;//getMonth returns 0-11, and we want 1-12; therefore, we add 1 to the returned value
+var year=mydate.getYear();
+
+if (year < 1000)
+	year += 1900;
+if (month < 10)
+	month = "0" + month;
+if (day<10)
+	day = "0" + day;
 
 //////EDIT below three variable to customize the format of the date/////
 
-var displayfirst=theyear
-var displaysecond=themonth
-var displaythird=theday
+var displayfirst=year;
+var displaysecond=month;
+var displaythird=day;
 
 ////////////////////////////////////
 
 //document.form1.enddate.value=displayfirst+"-"+displaysecond+"-"+displaythird
-document.getElementById('enddate').value = displayfirst+"-"+displaysecond+"-"+displaythird;
+document.getElementById('enddate').value = year + "-" + month + "-" + day;//Regardless of the format it displays, date inputs always store their value as yyyy-mm-dd
+
 function validateForm() {
-	var form1 = document.forms["form1"];
+	var form1 = document.forms["form1"];//Get the form with the name "form1" and store it in a variable to prevent stupid long lines of code
 
 	if(form1["gauge"].value == "Select...") {
 		alert("Please select a gauge");
 		return false;
 	}
-	if(form1["startdate"].value > form1["enddate"].value
-	  || form1["startdate"].value==null
-	  || form1["enddate"].value==null
-	){
+	if(form1["startdate"].value > form1["enddate"].value || form1["startdate"].value==null || form1["enddate"].value==null) {
 		alert("Please select valid dates");
 		return false;
 	}
@@ -38,4 +38,6 @@ function validateForm() {
 		form1["enddate"].value==date_create("2016-12-31 23:59:59",timezone_open("America/Toronto"));
 		return false;
 	}
+
+	return true;//If we get here, everything is a-ok!  Although there are probably better ways to prevent invalid inputs
 }
--- water-levels-app/build/generate.php
+++ water-levels-app/build/generate.php
@@ -1,9 +1,10 @@
 <?php
-	include 'HTMLReader.php';
+	include 'HTMLReader.php';//Include the HTMLReader class
 
-	$reader = new HTMLReader('generate.html');
+	$reader = new HTMLReader('generate.html');//Get the html file corresponding to this php file
 
-	$startDate = date("Y-m-d",strtotime('-2 week'));
-	$reader->insert('start-date', $startDate);
-	echo $reader->read();
+	$startDate = date("Y-m-d",strtotime('-2 week'));//Get the date value of 2 weeks ago
+
+	$reader->insert('start-date', $startDate);//Replace all instances of the <$start-date/> placeholder with the value stored in $startDate
+	echo $reader->read();//Display the modified contents of generate.html
 ?>
