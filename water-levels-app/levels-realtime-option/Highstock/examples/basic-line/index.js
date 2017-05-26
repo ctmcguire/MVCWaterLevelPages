764ee9ec4e5d205d5c149a3d088bc260d62b13f4
Water Management <water-management@mvc.local>
2017-05-26 11:14:59 -0400

    Split index.htm into htm and js files

---
+++ water-levels-app/levels-realtime-option/Highstock/examples/basic-line/index.js
@@ -0,0 +1,113 @@
+var seriesFull = [];
+var numData = 3;//Used for the loading message
+
+function makeData(tsURL, tsName, n) {
+	//These 2 variables are used to form the KiWIS URL to get data from
+	var prefix = "http://waterdata.quinteconservation.ca/KiWIS/KiWIS?service=kisters&type=queryServices&request=getTimeseriesValues&datasource=0&format=json&ts_id=";
+	var suffix = "&header=true&metadata=true&md_returnfields=station_name,parametertype_name&from=2005-01-01&dateformat=UNIX";
+
+	//Ajax query for data in JSON format
+	$.getJSON(prefix + tsURL + suffix, function (data) {
+
+		numData--;//Decrement the numData value each time the data is parsed
+
+		var data2 = data[0].data; //pointer to KVP parent object
+		var data3 = []; //will store updated KVP for each item
+
+		//Loop through date and param values and format for chart
+		for (var i = 1; i < data2.length; i++) {
+			//Get the date based on how requested in KiWIS, line below changes based on how you request the datestamp
+			data2[i][0] = parseFloat(data2[i][0]);
+			data2[i][1] = parseFloat(data2[i][1]); //Makes the param value a number
+			data3.push(data2[i]); //Push new KVP into data3
+		}
+
+		//populate the series for the chart
+
+		var singleSeries = {
+			_colorIndex: n,
+			_symbolIndex: seriesFull.length,
+			name: tsName,//Can also extract name directly from query
+			data: data3,
+			type: 'spline',
+			yAxis: n,
+			tooltip: {
+				valueSuffix: ' cms'
+			}
+		};
+
+		if(n == 2) {
+			singleSeries.type = 'column'
+			singleSeries.tooltip.valueSuffix = ' mm'
+		}
+		if(n == 0)
+			singleSeries.tooltip.valueSuffix = ' m'
+
+		seriesFull.push(singleSeries);
+
+		//Create and render the chart passing in the series
+		var chart = new Highcharts.StockChart({
+			chart: {renderTo: 'ChartContainer'},
+			legend: {enabled: true},
+			rangeSelector: {selected: 1},
+			title: {text: "Buckshot Creek Near Plevna"},
+			series: seriesFull,
+			xAxis: {ordinal: false},
+			yAxis: [
+				{ // Primary yAxis
+					labels: {
+						format: '{value} m',
+						style: {
+							color: Highcharts.getOptions().colors[0]
+						}
+					},
+					title: {
+						text: 'Water Level (m)',
+						style: {
+							color: Highcharts.getOptions().colors[0]
+						}
+					},
+					opposite: false
+				},
+				{ // Secondary yAxis
+					gridLineWidth: 0,
+					title: {
+						text: 'Flow Rate (cms)',
+						style: {
+							color: Highcharts.getOptions().colors[1]
+						}
+					},
+					labels: {
+						format: '{value} cms',
+						style: {
+							color: Highcharts.getOptions().colors[1]
+						}
+					},
+					opposite: false
+				},
+				{ // Tertiary yAxis
+					gridLineWidth: 0,
+					title: {
+						text: 'Precipitation (mm)',
+						style: {
+							color: Highcharts.getOptions().colors[2]
+						}
+					},
+					labels: {
+						format: '{value} mm',
+						style: {
+							color: Highcharts.getOptions().colors[2]
+						}
+					},
+					opposite: true
+				}
+			],
+			tooltip: {valueDecimals: 2,shared: true}
+		});
+
+		chart.showLoading();//Whichever data is loaded first displays the loading text (the others do it too, but the first one is the one that matters)
+
+		if(numData === 0)
+			chart.hideLoading();//Whichever data is loaded last hides the loading text
+	});
+}
