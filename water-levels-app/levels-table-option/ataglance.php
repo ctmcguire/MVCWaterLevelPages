b2d5b954536df40201917dbbac4b1516c015bf07
Water Management <water-management@mvc.local>
2017-05-26 14:59:46 -0400

    Commented ataglance.php/.html

--- water-levels-app/levels-table-option/ataglance.php
+++ water-levels-app/levels-table-option/ataglance.php
@@ -1,20 +1,25 @@
 <?php
-	include 'HTMLReader.php';
-	include 'TableLoader.php';
+	include 'HTMLReader.php';//include the HTMLReader class
+	include 'TableLoader.php';//include the TableLoader class
 
 	mysql_connect("localhost","mvconc55_levels1","4z9!yA");
 	mysql_select_db("mvconc55_mvclevels");
 
-	$table = new TableLoader();//Create an instance of a TableLoader object, to be used by the ataglance.php file
-	$reader = new HTMLReader('ataglance.html');
+	$table = new TableLoader();//Create an instance of a TableLoader object
+	$reader = new HTMLReader('ataglance.html');//Create an instance of an HTMLReader object, using the ataglance.html file
 
+	/* 
+	 * Get the rows for all of the gauges
+	 */
 	$flow = $table->getFlowData();
 	$daily = $table->getDailyLakeData();
 	$weekly = $table->getWeeklyLakeData();
 
+	/* 
+	 * Replace the placeholder tags with the rows for their respective gauges
+	 */
 	$reader->insert('flow-data', $flow);
 	$reader->insert('daily-data', $daily);
 	$reader->insert('weekly-data', $weekly);
-
-	echo $reader->read();
+	echo $reader->read();//display the html file
 ?>
