7ebf51321826c41740de0ce7f15678bbad35f3cb
Water Management <water-management@mvc.local>
2017-05-29 15:11:12 -0400

    Switched off of multiple small requests since they did not seem to increase performance by a large margin, and I was uncertain of how KiWIS would handle the page being visited by multiple people at once.

--- water-levels-app/levels-realtime-option/Highstock/examples/basic-line/index.js
+++ water-levels-app/levels-realtime-option/Highstock/examples/basic-line/index.js
@@ -81,86 +81,55 @@ function makeChart() {
  * 					makeData('1391042', 'Water Level', 0);
  * The above example gets the water level data for the gauge at Buckshot Creek (near plevna)
 **/
-function makeData(tsURL, tsName, n) {
-	const FROM = 1994;
-	const TO = 2017;
-
-	if(chart === null)
-		makeChart();
-	if(chart === null) {
-		console.log('A landslide has occurred!  A landslide has occurred!');
-	}
-
-	//These 2 variables are combined with tsURL to form the KiWIS URL from which data is retrieved
+function makeData(url, tsName, n) {
+	//These 2 variables are combined with url to form the KiWIS URL from which data is retrieved
 	var prefix = "http://waterdata.quinteconservation.ca/KiWIS/KiWIS?service=kisters&type=queryServices&request=getTimeseriesValues&datasource=0&format=json&ts_id=";
-	var suffix = "&header=true&metadata=true&md_returnfields=station_name,parametertype_name&dateformat=UNIX";
+	var suffix = "&header=true&metadata=true&md_returnfields=station_name,parametertype_name&dateformat=UNIX&from=1994-01-01";
 
-	var yearlyData = [];
+	numData++;//Keep track of how many times makeDataByYear is called in order to properly show/hide 'loading...' text
+	$.getJSON(prefix + url + suffix, function(data) {
 
-	function makeDataByYear(url, j) {
-		var year = '&from=' + j + '-01-01&to=' + j + '-12-31';
-		numData++;//Keep track of how many times makeDataByYear is called in order to properly show/hide 'loading...' text
-		$.getJSON(url+year, function(data) {
+		numData--;//Decrement the numData value each time the data is parsed; will be at zero once the final data series executes this line
 
-			numData--;//Decrement the numData value each time the data is parsed; will be at zero once the final data series executes this line
+		var data2 = data[0].data; //pointer to KVP parent object
+		var data3 = []; //will store updated KVP for each item
 
-			var dothething = true;
-			var finalData = [];
+		//Loop through date and param values and format for chart
+		for (var i = 1; i < data2.length; i++) {
+			//Get the date based on how requested in KiWIS, line below changes based on how you request the datestamp
+			data2[i][0] = parseFloat(data2[i][0]);
+			data2[i][1] = parseFloat(data2[i][1]); //Makes the param value a number
+			data3.push(data2[i]); //Push new KVP into data3
+		}
 
-			var data2 = data[0].data; //pointer to KVP parent object
-			var data3 = []; //will store updated KVP for each item
-
-			//Loop through date and param values and format for chart
-			for (var i = 1; i < data2.length; i++) {
-				//Get the date based on how requested in KiWIS, line below changes based on how you request the datestamp
-				data2[i][0] = parseFloat(data2[i][0]);
-				data2[i][1] = parseFloat(data2[i][1]); //Makes the param value a number
-				data3.push(data2[i]); //Push new KVP into data3
+		//populate the series for the chart (type and tooltip default to their values for flow rate)
+		var singleSeries = {
+			_colorIndex: n,
+			_symbolIndex: seriesFull.length,
+			name: tsName,//Can also extract name directly from query
+			data: data3,
+			type: 'spline',//Both flow rate and water level use line charts
+			yAxis: n,
+			tooltip: {
+				valueSuffix: ' cms'//flow rate is measured in cubic meters per second (which should really be m^3/s, but I guess that confuses people?)
 			}
+		};
 
-			yearlyData[j] = data3;
-			for(i = FROM; i <= TO; i++) {
-				if(yearlyData[i] == null)
-					return;
-				finalData = finalData.concat(yearlyData[i]);
-			}
-			//finalData = data3;
+		if(n === 2) {
+			singleSeries.type = 'column';//Precipitation uses a bar chart, instead of a line chart
+			singleSeries.tooltip.valueSuffix = ' mm';//Precipitation is measured in millimeters
+		}
+		if(n === 0)
+			singleSeries.tooltip.valueSuffix = ' m';//Water levels are measured in meters
 
-			//populate the series for the chart (type and tooltip default to their values for flow rate)
-			var singleSeries = {
-				_colorIndex: n,
-				_symbolIndex: seriesFull.length,
-				name: tsName,//Can also extract name directly from query
-				data: finalData,
-				type: 'spline',//Both flow rate and water level use line charts
-				yAxis: n,
-				tooltip: {
-					valueSuffix: ' cms'//flow rate is measured in cubic meters per second (which should really be m^3/s, but I guess that confuses people?)
-				}
-			};
+		seriesFull.push(singleSeries);//Add the current series to the array of serieses
 
-			if(n === 2) {
-				singleSeries.type = 'column';//Precipitation uses a bar chart, instead of a line chart
-				singleSeries.tooltip.valueSuffix = ' mm';//Precipitation is measured in millimeters
-			}
-			if(n === 0)
-				singleSeries.tooltip.valueSuffix = ' m';//Water levels are measured in meters
+		if(chart === null)
+			makeChart();//Make the chart if it doesn't already exist
+		else
+			chart.addSeries(singleSeries);//Otherwise, add the series to the already existing chart
 
-			seriesFull.push(singleSeries);//Add the current series to the array of serieses
-
-			//Create and render the chart passing in the series
-
-			chart.addSeries(singleSeries);
-			//chart.showLoading();//Whichever data is loaded first displays the loading text (the others do it too, but the first one is the one that matters)
-
-			if(numData === 0)
-				chart.hideLoading();//Whichever data is loaded last hides the loading text
-			//console.log(j);
-		});
-	}
-
-	//Ajax query for data in JSON format
-	for(var i = FROM; i <= TO; i++) {
-		makeDataByYear(prefix + tsURL + suffix, i);
-	}
+		if(numData === 0)
+			chart.hideLoading();//Whichever data is loaded last hides the loading text
+	});
 }
