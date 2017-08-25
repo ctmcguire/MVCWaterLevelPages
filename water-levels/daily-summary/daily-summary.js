96fdf37031637200336041624ffef04e1ebe6857
Water Management <water-management@mvc.local>
2017-08-25 11:58:14 -0400

    Removed jQuery dependency

--- water-levels/daily-summary/daily-summary.js
+++ water-levels/daily-summary/daily-summary.js
@@ -1,32 +1,34 @@
 /**
- * This function displays the tooltip for the target element
-**/
-function showToolTip(e) {
-	var target = $(e.target);
-	if(target.hasClass('show'))
-		return;
-	target.addClass('show');
-}
-/**
- * This function hides the tooltip for the target element
-**/
-function hideToolTip(e) {
-	var target = $(e.target);
-	if(!target.hasClass('show'))
-		return;
-	target.removeClass('show');
-}
-
-/**
  * This function sets up the event handlers
- * 
- * @return	- false if jQuery is not included
- * @return	- true otherwise
 **/
-function setupHandlers() {
-	if(window.$ === undefined)
-		return false;//currently this is dependent on jQuery (I may remove this dependency in the future)
-	$("p.tooltip").on('click', showToolTip);//set up the click event handler
-	$("p.tooltip").on('mouseout', hideToolTip);//set up the mouseout event handler
-	return true;
-}
+(function() {
+	var tars = document.querySelectorAll('p.tooltip');
+	for(var i = 0; i < tars.length; i++) {
+		/**
+		 * This function displays the tooltip for the target element
+		 * 
+		 * @param e	- event that triggered this function
+		**/
+		tars[i].onclick = function(e) {
+			if(e.target === undefined)
+				return;//exit if the event had no target
+			var classes = e.target.classList;
+			if(classes.contains('show'))
+				return;//do nothing if the target element already has the show class
+			classes.add('show');//add the show class
+		};
+		/**
+		 * This function hides the tooltip for the target element
+		 * 
+		 * @param e	- event that triggered this function
+		**/
+		tars[i].onmouseout = function(e) {
+			if(e.target === undefined)
+				return;
+			var classes = e.target.classList;
+			if(!classes.contains('show'))
+				return;
+			classes.remove('show');
+		};
+	}
+})();
