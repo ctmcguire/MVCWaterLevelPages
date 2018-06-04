28d6ea6b4589f9a8c8375743fd134a32cbf6f635
Water- Management <Water-Management@mvc18.local>
2018-06-04 09:04:45 -0400

    Added legal pages

---
+++ legal/terms-of-use/mapping-apps/index.php
@@ -0,0 +1,9 @@
+<?php
+	include $_SERVER['DOCUMENT_ROOT'].'/classes/HTMLReader.php';//Include the HTMLReader class
+	include $_SERVER['DOCUMENT_ROOT'].'/classes/CustomTag.php';//include CustomTag if not already included
+	include $_SERVER['DOCUMENT_ROOT'].'/header-footer/header-footer.php';
+
+	$reader = new HTMLReader('mapping-apps.html', 'Mapping Applications Terms of Use');//Get the html file corresponding to this php file
+	addHeaderFooter($reader);//Add the header and footer to this page
+	echo $reader->read();//Display the modified contents of generate.html
+?>
