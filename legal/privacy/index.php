43834455e3dcdcd7b9d0f563a2cdecb27c503341
Water- Management <Water-Management@mvc18.local>
2018-06-06 10:56:06 -0400

    Fixed issue with page title

--- legal/privacy/index.php
+++ legal/privacy/index.php
@@ -3,7 +3,7 @@
 	include $_SERVER['DOCUMENT_ROOT'].'/classes/CustomTag.php';//include CustomTag if not already included
 	include $_SERVER['DOCUMENT_ROOT'].'/header-footer/header-footer.php';
 
-	$reader = new HTMLReader('privacy.html', 'Privacy');//Get the html file corresponding to this php file
-	addHeaderFooter($reader);//Add the header and footer to this page
+	$reader = new HTMLReader('privacy.html');//Get the html file corresponding to this php file
+	addHeaderFooter($reader, 'Privacy');//Add the header and footer to this page
 	echo $reader->read();//Display the modified contents of generate.html
 ?>
