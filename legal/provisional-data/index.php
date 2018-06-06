3a58288d53d449041cecac4f1347e15296b970ac
Water- Management <Water-Management@mvc18.local>
2018-06-06 10:57:49 -0400

    Moved provisional data page into legal folder; performed the same adjustments as those made to the other legal pages over the past 2 commits; changed stylesheet to the uniform legal one and removed old stylesheet

--- legal/provisional-data/index.php
+++ legal/provisional-data/index.php
@@ -3,7 +3,7 @@
 	include $_SERVER['DOCUMENT_ROOT'].'/classes/CustomTag.php';//include CustomTag if not already included
 	include $_SERVER['DOCUMENT_ROOT'].'/header-footer/header-footer.php';
 
-	$reader = new HTMLReader('provisional-data.html', 'Provisional Data');//Get the html file corresponding to this php file
-	addHeaderFooter($reader);//Add the header and footer to this page
+	$reader = new HTMLReader('provisional-data.html');//Get the html file corresponding to this php file
+	addHeaderFooter($reader, 'Provisional Data');//Add the header and footer to this page
 	echo $reader->read();//Display the modified contents of generate.html
 ?>
